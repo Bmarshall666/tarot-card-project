739200d1a678a0b384e970c5af5e5416387b1337
BrandonMarshall <brandonmarshall666@gmail.com>
2024-06-19 14:26:18 +0100

    Sorted CSS

--- src/App.vue
+++ src/App.vue
@@ -1,169 +1,92 @@
 <script setup>
-import { reactive, ref } from "vue";
-import axios from "axios"
-
-const randCardNum = Math.floor(Math.random() * 58);
-const randMeaningNum = Math.floor(Math.random() * 3);
-
-axios.get('tarot-images.json')
-  .then((data) => {
-    resultState.name = data.data.cards[randCardNum].name
-    resultState.number = data.data.cards[randCardNum].number
-    resultState.arcana = data.data.cards[randCardNum].arcana
-    resultState.meaning_rev = data.data.cards[randCardNum].meanings.light[randMeaningNum]
-    resultState.suit = data.data.cards[randCardNum].suit
-    resultState.img = data.data.cards[randCardNum].img
-    resultState.Archetype = data.data.cards[randCardNum].Archetype
-  });
-
-const clicked = ref(false)
-const isFlipped = ref(false)
-
-const resultState = reactive({
-  name: "",
-  number: "",
-  arcana: "",
-  meaning_rev: "",
-  suit: "",
-  img: "",
-  Archetype: ""
-})
-
-const getTarot = () => {
-  const randCardNum = Math.floor(Math.random() * 58);
-  const randMeaningNum = Math.floor(Math.random() * 3);
-
-  axios.get('tarot-images.json')
-    .then((data) => {
-      resultState.name = data.data.cards[randCardNum].name
-      resultState.number = data.data.cards[randCardNum].number
-      resultState.arcana = data.data.cards[randCardNum].arcana
-      resultState.meaning_rev = data.data.cards[randCardNum].meanings.light[randMeaningNum]
-      resultState.suit = data.data.cards[randCardNum].suit
-      resultState.img = data.data.cards[randCardNum].img
-      resultState.Archetype = data.data.cards[randCardNum].Archetype
-    });
-  clicked.value = false
-  isFlipped.value = false
-}
-
-const isClicked = () => {
-  clicked.value = true
-  isFlipped.value = !isFlipped.value
-}
-
+import DisplayCard from './components/DisplayCard.vue';
 </script>
 <template>
   <div class="title">
     <h2>Welcome</h2>
-    <div>Click to reveal your card</div>
+    <div class="">Click to reveal your card</div>
   </div>
-  <section>
-    <div class="card">
+  <div class="container">
+    <DisplayCard ref="cardRef" @card-info="cardInfo" />
+    <button @click="$refs.cardRef.getTarot()">Retry</button>
 
-      <transition name="fade-card">
-        <div v-if="isFlipped === false" class="inner-card">
-          <img src="/src/assets/cards/cardBack.jpg" @click="isClicked" class="cardBack">
-        </div>
-      </transition>
+  </div>
 
-      <transition name="fade-card">
-        <div v-if="isFlipped" class="inner-card">
-          <img v-bind:src="'/src/assets/cards/' + resultState.img" @click="isClicked" class="cardFront">
-        </div>
-      </transition>
-
-      <div class="card-info">
-        <transition name="fade">
-          <div v-if="clicked">
-            <div>{{ resultState.name }}</div>
-            <div>{{ resultState.meaning_rev }}</div>
-          </div>
-        </transition>
-
-      </div>
-    </div>
-    <button class="retry" @click="getTarot">Retry</button>
-  </section>
 </template>
 
 <style scoped>
 h2 {
-  margin: 1vh;
+  margin: 0.5rem;
+}
+
+.container {
+  position: relative;
+  display: inline;
+  align-items: center;
+  justify-content: center;
+  top: 50vh;
 }
 
 .title {
-  margin: 5vh;
-  
-}
-
-.card {
   position: relative;
-  padding: 5px;
-  display: flex;
-  place-items: center;
-  align-items: center;
-}
-
-.inner-card {
-  position: absolute;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 15vh;
-  left: 0;
-  right: 0;
+  max-width: 15rem;
   text-align: center;
-
-}
-
-.card-info {
-  color: black;
-  background-color: #f1f1f1;
-  min-width: 30rem;
-  min-height: 5rem;
-  border-radius: 26px;
-  padding: 0;
-  margin: 0;
-  text-align: center;
-  align-items: center;
-  margin-top: 45vh;
-  position: relative;
-}
-
-.cardFront {
-  max-height: 25rem;
-  width: 16rem;
-  border-radius: 22px;
-  max-width: 20.5rem;
-  object-position: 5px;
+  left: calc(50vw - 120px);
+  top: calc(50vh - 220px);
 }
 
 button {
-  margin: .3rem;
+  position: absolute;
+  color: black;
+  border-radius: 8px;
+  border: 3px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #f9f9f9;
+  cursor: pointer;
+  transition: border-color 0.25s;
+  left: calc(50vw - 40px);
+  top: calc(25vh - 15px);
 }
 
-.cardBack {
-  max-width: 15.5rem;
-  border-radius: 24px;
+button:hover {
+  border-color: #8d90bb;
 }
 
-.fade-card-enter-active,
-.fade-card-leave-active {
-  transition: opacity 1.5s ease;
+button:focus,
+button:focus-visible {
+  border-color: 4px auto #8d90bb;
 }
 
-.fade-card-enter-from,
-.fade-card-leave-to {
-  opacity: 0;
+
+@media (max-width:856px) {
+  .container {
+    position: relative;
+    display: inline;
+    align-items: center;
+    justify-content: center;
+    top: calc(50vh - 100px);
+  }
+  
+
+  button {
+    position: absolute;
+    color: black;
+    border-radius: 8px;
+    border: 3px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #f9f9f9;
+    cursor: pointer;
+    transition: border-color 0.25s;
+    left: calc(50vw - 40px);
+    top: calc(25vh - 5px);
+  }
 }
 
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 1.5s ease;
-}
 
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
 </style>
---
+++ src/components/DisplayCard.vue
@@ -0,0 +1,169 @@
+<script setup>
+import { reactive, ref } from "vue";
+import axios from "axios";
+
+const clicked = ref(false)
+const isFlipped = ref(false)
+
+const randCardNum = Math.floor(Math.random() * 58);
+const randMeaningNum = Math.floor(Math.random() * 3);
+
+const resultState = reactive({
+    name: "",
+    number: "",
+    arcana: "",
+    meaning_rev: "",
+    suit: "",
+    img: "",
+    Archetype: ""
+})
+
+const emit = defineEmits(["cardInfo"])
+
+axios.get('tarot-images.json')
+    .then((data) => {
+        resultState.name = data.data.cards[randCardNum].name
+        resultState.number = data.data.cards[randCardNum].number
+        resultState.arcana = data.data.cards[randCardNum].arcana
+        resultState.meaning_rev = data.data.cards[randCardNum].meanings.light[randMeaningNum]
+        resultState.suit = data.data.cards[randCardNum].suit
+        resultState.img = data.data.cards[randCardNum].img
+        resultState.Archetype = data.data.cards[randCardNum].Archetype
+    });
+
+
+const getTarot = () => {
+    const randCardNum = Math.floor(Math.random() * 58);
+    const randMeaningNum = Math.floor(Math.random() * 3);
+
+    axios.get('tarot-images.json')
+        .then((data) => {
+            resultState.name = data.data.cards[randCardNum].name
+            resultState.number = data.data.cards[randCardNum].number
+            resultState.arcana = data.data.cards[randCardNum].arcana
+            resultState.meaning_rev = data.data.cards[randCardNum].meanings.light[randMeaningNum]
+            resultState.suit = data.data.cards[randCardNum].suit
+            resultState.img = data.data.cards[randCardNum].img
+            resultState.Archetype = data.data.cards[randCardNum].Archetype
+        });
+    clicked.value = false
+    isFlipped.value = false
+}
+
+const isClicked = () => {
+    clicked.value = true
+    isFlipped.value = !isFlipped.value
+}
+
+defineExpose({ getTarot })
+
+
+
+</script>
+
+<template>
+    <div class="card">
+        <transition name="fade-card">
+            <div v-if="isFlipped === false" class="inner-card">
+                <img src="/src/assets/cards/cardBack.jpg" @click="isClicked" class="cardBack">
+            </div>
+        </transition>
+
+        <transition name="fade-card">
+            <div v-if="isFlipped" class="inner-card">
+                <img v-bind:src="'/src/assets/cards/' + resultState.img" @click="isClicked" class="cardFront">
+            </div>
+        </transition>
+        <div class="card-Info-Container">
+            <transition name="fade">
+                <div v-if="clicked" class="card-Info">
+                    <div>{{ resultState.name }}</div>
+                    <div>{{ resultState.meaning_rev }}</div>
+                </div>
+            </transition>
+        </div>
+    </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+.card {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.cardFront {
+    height: 25rem;
+    width: 16rem;
+    border-radius: 22px;
+    max-width: 20.5rem;
+    object-position: 5px;
+}
+
+.cardBack {
+    max-width: 15.5rem;
+    border-radius: 24px;
+}
+
+.inner-card {
+    position: absolute;
+    place-items: center;
+    align-items: center;
+}
+
+.card-Info-Container {
+    position: absolute;
+    top: calc(18vh - 10px);
+}
+
+.card-Info {
+    color: black;
+    background-color: #f1f1f1;
+    text-align: center;
+    position: relative;
+    min-width: 100vw;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 1.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.fade-card-enter-active,
+.fade-card-leave-active {
+    transition: opacity 1.5s ease;
+}
+
+.fade-card-enter-from,
+.fade-card-leave-to {
+    opacity: 0;
+}
+
+@media (max-width:856px) {
+    .cardFront {
+        height: 12.5rem;
+        width: 8rem;
+        border-radius: 22px;
+        max-width: 20.5rem;
+        object-position: 5px;
+    }
+
+    .cardBack {
+        max-width: 7.75rem;
+        border-radius: 24px;
+    }
+
+    .card-Info-Container {
+        position: absolute;
+        top: calc(18vh - 20px);
+    }
+}
+</style>
